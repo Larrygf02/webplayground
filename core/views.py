fde0e3211426b17451ba66ec5323dda6fc4a2979
larrygf02 <raulgf_02@hotmail.com>
2019-03-01 20:58:19 -0500

    App core

--- core/views.py
+++ core/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-def home(request):
-    return render(request, "core/home.html")
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
+    """def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Playground'
+        return context"""
+    def get(self, request, *args, **kwargs):
+        context = {
+            'title': 'Web Playground '
+        }
+        return render(request, self.template_name, context)
 
-def sample(request):
-    return render(request, "core/sample.html")
+class SamplePageView(TemplateView):
+    template_name = "core/sample.html"
