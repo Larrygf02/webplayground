45ab59e8bc2b243c31e324ca88bccb97b0434f08
larrygf02 <raulgf_02@hotmail.com>
2019-03-01 22:19:36 -0500

    CBV +1

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse, reverse_lazy
 from .models import Page
 
 
@@ -10,3 +12,10 @@ class PageListView(ListView):
 
 class PageDetailView(DetailView):
     model = Page
+
+class PageCreateView(CreateView):
+    model = Page
+    fields = ['title', 'content','order']
+    success_url = reverse_lazy('pages:pages') 
+    """def get_success_url(self):
+        return reverse('pages:pages')"""
