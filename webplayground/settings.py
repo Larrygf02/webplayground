5218c67fc15972564e6133f9058cc2dabcc12fab
larrygf02 <raulgf_02@hotmail.com>
2019-03-02 12:52:55 -0500

    Authenticacion con CBV

--- webplayground/settings.py
+++ webplayground/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'registration', #dar prioridad sobre todas las apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+#auth redirect
+LOGIN_REDIRECT_URL = 'home'
