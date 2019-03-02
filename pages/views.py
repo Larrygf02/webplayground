034ea3dfefd2c7a0954667cca1e2cae5807960f4
larrygf02 <raulgf_02@hotmail.com>
2019-03-02 11:41:03 -0500

    Diseño para los formularios CRUD

--- pages/views.py
+++ pages/views.py
@@ -4,7 +4,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
 from .models import Page
-
+from .forms import PageForm
 
 # Create your views here.
 class PageListView(ListView):
@@ -15,14 +15,12 @@ class PageDetailView(DetailView):
 
 class PageCreateView(CreateView):
     model = Page
-    fields = ['title', 'content','order']
+    form_class = PageForm
     success_url = reverse_lazy('pages:pages') 
-    """def get_success_url(self):
-        return reverse('pages:pages')"""
 
 class PageUpdate(UpdateView):
     model = Page
-    fields = ['title','content', 'order']
+    form_class = PageForm
     template_name_suffix = '_update_form'
 
     def get_success_url(self):
