d5a8de55855c354089e5db037fa6570f8b4c230a
larrygf02 <raulgf_02@hotmail.com>
2019-03-09 11:27:59 -0500

    Vistas para el app messenger

--- webplayground/urls.py
+++ webplayground/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from pages.urls import pages_patterns
 from profiles.urls import profiles_patterns
+from messenger.urls import messenger_patterns
 from django.conf import settings
 
 urlpatterns = [
@@ -26,7 +27,8 @@ urlpatterns = [
     #paths de auth
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('registration.urls')),
-    path('profiles/', include(profiles_patterns))
+    path('profiles/', include(profiles_patterns)),
+    path('messenger/', include(messenger_patterns)),
 ]
 #configurar las urls para la carga de archivos media
 if settings.DEBUG:
