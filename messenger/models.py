1877c141eae08aeecc059db561fbc913460e53af
larrygf02 <raulgf_02@hotmail.com>
2019-03-09 10:11:41 -0500

    TDD Refactorizacion

--- messenger/models.py
+++ messenger/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import m2m_changed
 
 # Create your models here.
 class Message(models.Model):
@@ -10,5 +11,23 @@ class Message(models.Model):
         ordering = ['created']
 
 class Thread(models.Model):
-    user = models.ManyToManyField(User, related_name='threads')
+    users = models.ManyToManyField(User, related_name='threads')
     messages = models.ManyToManyField(Message)
+
+def messages_changed(sender, **kwargs):
+    instance = kwargs.pop("instance", None)
+    action = kwargs.pop("action", None)
+    pk_set = kwargs.pop("pk_set", None)
+    print(instance, action, pk_set)
+
+    false_pk_set = set()
+    if action is "pre_add":
+        for msg_pk in pk_set:
+            msg = Message.objects.get(pk=msg_pk)
+            if msg.user not in instance.users.all():
+                print("Ups, ({}) no forma parte del hilo".format(msg.user))
+                false_pk_set.add(msg_pk)
+    #Buscamos los mensajes fraudulentos y los eliminamos
+    pk_set.difference_update(false_pk_set) #difference_update metodo de set para diferencia conjuntos
+
+m2m_changed.connect(messages_changed, sender=Thread.messages.through)
